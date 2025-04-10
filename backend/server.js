e822e16897c5aa9d0f2ed5b366f19fcef9671c7b
suraj rawat <surajrwat001@gmail.com>
2025-04-10 08:36:32 +0530

    update cors

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,10 @@ connectCloudinary()
 
 // middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: 'https://finalprescripto.netlify.app',
+  credentials: true,
+}))
 
 // api endpoints
 app.use("/api/user", userRouter)
