97c6830ebd961b4766fc8ef210487e1c9e901616
suraj rawat <surajrwat001@gmail.com>
2025-04-10 08:56:47 +0530

    update vite config file

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: { port: 5174 },
+  base: './', // 👈 Required for correct asset loading on Netlify
   build: {
     rollupOptions: {
-      external: ['axios'],
+      external: ['axios'], // Keeps build from breaking if axios isn't resolved
     },
   },
 })
